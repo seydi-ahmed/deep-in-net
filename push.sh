fa33e74cd3aef6a559a70f8dd1cf29ee3486a5d7
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-03-05 08:11:24 +0000

    hub: function, operation and role in networking

--- push.sh
+++ push.sh
@@ -3,4 +3,4 @@ git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
 git commit -m "hub: function, operation and role in networking"
-git push --force
+git push force
